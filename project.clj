08e5d5845c2eccec5f4f22209109569ecafa3df6
Mark Bastian <markbastian@gmail.com>
2020-07-18 22:40:21 -0600

    Ready to release.

--- project.clj
+++ project.clj
@@ -4,45 +4,16 @@
   :url "https://github.com/markbastian/planning"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :repositories [["releases" {:url "https://repo.clojars.org" :creds :gpg}]
+                 ["snapshots" {:url "https://repo.clojars.org" :creds :gpg}]]
+  
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/data.priority-map "1.0.0"]]
+                 [org.clojure/data.priority-map "1.0.0"]
+                 [org.clojure/clojurescript "1.10.773" :scope "provided"]
+                 [tailrecursion/cljs-priority-map "1.2.1"]]
 
   :jar-exclusions [#"\.swp|\.swo|\.DS_Store"]
 
-  :aliases {"min-build" ["with-profile" "cljs" "cljsbuild" "once" "min"]
-            "fig" ["with-profile" "cljs" "figwheel"]}
-
-  :profiles {:uberjar {:aot :all}
-             :dev     {:plugins      [[lein-figwheel "0.5.20"]
-                                      [lein-cljsbuild "1.1.7"]
-                                      [org.clojure/clojurescript "1.10.439"]]
-                       :dependencies [[cider/piggieback "0.5.0"]
-                                      [figwheel-sidecar "0.5.20"]]
-                       :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}}
-             :cljs    {:plugins      [[lein-cljsbuild "1.1.7"]]
-                       :dependencies [[org.clojure/clojurescript "1.10.773"]
-                                      [tailrecursion/cljs-priority-map "1.2.1"]
-                                      [reagent "0.10.0"]
-                                      [cljsjs/hammer "2.0.8-0"]]}}
-
   :source-paths ["src/main/clj" "src/main/cljc"]
-  :test-paths ["src/test/clj" "src/test/cljc"]
-
-  :repl-options {:init-ns planning.core}
-
-  :cljsbuild {:builds
-              {:dev {:source-paths ["src/main/cljs" "src/main/cljc"]
-                     :figwheel     true
-                     :compiler     {:main                 planning.app
-                                    :asset-path           "js/out"
-                                    :output-to            "resources/public/js/planning.js"
-                                    :output-dir           "resources/public/js/out"
-                                    :source-map-timestamp true
-                                    :pretty-print         true}}
-
-               :min {:source-paths ["src/main/cljs" "src/main/cljc"]
-                     :compiler     {:main          planning.app
-                                    :output-to     "resources/public/js/planning.js"
-                                    :optimizations :advanced
-                                    :pretty-print  false}}}}
-  :figwheel {:css-dirs ["resources/public/css"]})
+  :test-paths ["src/test/clj" "src/test/cljc"])
