291df1236b90b384beed317d3944c29ab8cf5aee
Mark Bastian <markbastian@gmail.com>
2020-07-19 07:35:05 -0600

    Trying to get releases to work.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject
-  planning "0.1.0-SNAPSHOT"
+  markbastian/planning "0.1.0-SNAPSHOT"
   :description "A planning library"
   :url "https://github.com/markbastian/planning"
   :license {:name "Eclipse Public License"
@@ -7,7 +7,7 @@
 
   :repositories [["releases" {:url "https://repo.clojars.org" :creds :gpg}]
                  ["snapshots" {:url "https://repo.clojars.org" :creds :gpg}]]
-  
+
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.priority-map "1.0.0"]
                  [org.clojure/clojurescript "1.10.773" :scope "provided"]
